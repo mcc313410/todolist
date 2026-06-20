bb72cdd9b7554f1585cb5408545b0796e6df1a09
mcc <3445956933@qq.com>
2026-06-20 16:59:54 +0800

    最终版3,主题颜色变了一点

--- app/src/main/java/com/example/todolist/base/BaseActivity.java
+++ app/src/main/java/com/example/todolist/base/BaseActivity.java
@@ -1,14 +1,12 @@
 package com.example.todolist.base;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.todolist.utils.ThemeUtil;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ThemeUtil.applyTheme(this);
         super.onCreate(savedInstanceState);
+        // 预留位置，后续可以添加全局通用逻辑
     }
 }
